e6f815851d0513e07076ac08f9ee98a85b9e0c88
Pavel Lobashov <shockwavenn@gmail.com>
2021-02-08 23:34:58 +0300

    Finished lesson 13

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   get 'about', to: 'about#index'
 
+  get 'sign_up', to: 'registrations#new'
+  post 'users', to: 'registrations#create'
+
   root to: 'main#index'
 end
